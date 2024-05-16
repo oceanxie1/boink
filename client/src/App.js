0e4360c85efc3238390d926c9b66a82be876c3cf
Ocean Xie <oceanxie1@gmail.com>
2024-05-16 10:47:52 -0500

    initialized react project and backend and more

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import { Home } from "@material-ui/icons";
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Location,
+  useLocation
+} from 'react-router-dom'
+import Home from "./scene/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path = "/" element = {<Home />} />
+          <Route path = "item/:itemId" element = {<ItemDetails />} />
+          <Route path = "checkout" element = {<Checkout />} />
+          <Route path = "checkout/success" element = {<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/theme.js
@@ -0,0 +1,75 @@
+import { createTheme } from '@mui/material/styles'
+
+export const shades = {
+    primary: {
+          100: "#f9eeee",
+          200: "#f3dede",
+          300: "#edcdcd",
+          400: "#e7bdbd",
+          500: "#e1acac",
+          600: "#b48a8a",
+          700: "#876767",
+          800: "#5a4545",
+          900: "#2d2222"
+    },  
+
+    secondary: {
+        100: "#fffbf0",
+        200: "#fff7e0",
+        300: "#fff3d1",
+        400: "#ffefc1",
+        500: "#ffebb2",
+        600: "#ccbc8e",
+        700: "#998d6b",
+        800: "#665e47",
+        900: "#332f24"
+  },
+
+    neutral: {
+        100: "#faf9f4",
+        200: "#f6f2ea",
+        300: "#f1ecdf",
+        400: "#ede5d5",
+        500: "#e8dfca",
+        600: "#bab2a2",
+        700: "#8b8679",
+        800: "#5d5951",
+        900: "#2e2d28"
+    },
+}
+
+export const themes = createTheme({
+    palette: {
+        primary: {
+            main: shades.primary[500]
+        },
+        secondary: {
+            main: shades.secondary[500]
+        },
+        neutral: {
+            dark: shades.neutral[700],
+            main: shades.neutral[500],
+            light: shades.neutral[100]
+        }
+    },
+    typography: {
+        fontFamily: ["Danfo", "sans-serif"].join(","),
+        fontSize: 11,
+        h1: {
+            fontFamily: ["Danfo", "sans-serif"].join(","),
+        fontSize: 48,
+        },
+        h2: {
+            fontFamily: ["Danfo", "sans-serif"].join(","),
+        fontSize: 36,
+        },
+        h3: {
+            fontFamily: ["Danfo", "sans-serif"].join(","),
+        fontSize: 20,
+        },
+        h4: {
+            fontFamily: ["Danfo", "sans-serif"].join(","),
+        fontSize: 14,
+        },
+    }
+})
